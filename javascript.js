4595ea2f1d3841d278ef9b10d01f4dbb4a905b1b
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-12-03 21:03:31 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,5 +2,16 @@
 const patterns = {
         telephone: /^\d{11}$/,
         vladimir : /vladimir/,
-        number : /[0-9]{11}
+        number : /[0-9]{11},
+        username: /^[a-z\d]{5,12}$/i,
+        password: /^[\d\w@-]{8,20}$/i
 };
+
+const inputs = document.querySelectorAll('input');
+
+// attach keyup events to inputs
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+             console.log(patterns[e.target.attributes.name.value]);
+    });
+});
